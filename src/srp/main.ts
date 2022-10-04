f74581b9ff7dc16697377900e31d87204bfc482b
Rodrigo Gomes Calixto da Silva <rodrigo.calixto@radixeng.com.br>
2022-10-04 15:40:12 -0300

    dry - dont repeat yourself

--- src/srp/main.ts
+++ src/srp/main.ts
@@ -1,12 +1,12 @@
-import { NoDiscount } from './entities/discount';
+import { TenPercentDiscount } from './entities/discount';
 import { Messaging } from './services/messaging';
 import { Order } from './entities/order';
 import { Persistency } from './services/persistency';
 import { Product } from './entities/product';
 import { ShoppingCart } from './entities/shopping-cart';
 
-const noDiscount = new NoDiscount();
-const shoppingCart = new ShoppingCart(noDiscount);
+const tenPercentDiscount = new TenPercentDiscount();
+const shoppingCart = new ShoppingCart(tenPercentDiscount);
 shoppingCart.addItem(new Product('Camiseta', 49.9));
 shoppingCart.addItem(new Product('Caderno', 9.9));
 shoppingCart.addItem(new Product('Lápis', 1.59));
